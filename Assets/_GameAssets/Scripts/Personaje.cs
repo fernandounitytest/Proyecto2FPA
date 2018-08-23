4ffee51cc643c71060def2ca4655476d9da448de
fernandopaniagua <fernando.paniagua@gmail.com>
2018-08-23 21:01:45 +0200

    Commit parcial

---
+++ Assets/_GameAssets/Scripts/Personaje.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Personaje : MonoBehaviour {
+    enum Estado { Idle, ToRight, ToLeft};
+    [SerializeField] int vidaMaxima;
+    bool estaVivo = true;
+    private int vidaActual;
+
+    public int VidaActual
+    {
+        get
+        {
+            return vidaActual;
+        }
+
+        set
+        {
+            vidaActual = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
